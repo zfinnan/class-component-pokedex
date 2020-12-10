a90f4859bd44bb8c270f94cc0f44947a2b696dc8
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-12-10 16:25:02 -0500

    add componentdidupdate

---
+++ src/Components/Pokedex.js
@@ -0,0 +1,46 @@
+import { Component } from 'react'
+import Axios from 'axios'
+
+class Pokedex extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            pokemonName: 'pikachu',
+            pokemonImage: ''
+        }
+    }
+
+    componentDidMount() {
+        Axios.get(`https://pokeapi.co/api/v2/pokemon/${this.state.pokemonName}`).then((res) => {
+            this.setState({ 
+                pokemonImage: res.data.sprites.front_default
+            })
+        })
+    }
+
+    componentDidUpdate() {
+        if (this.state.pokemonName === '') {
+            return
+        }
+        Axios.get(`https://pokeapi.co/api/v2/pokemon/${this.state.pokemonName}`).then((res) => {
+            this.setState({ 
+                pokemonImage: res.data.sprites.front_default
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Fischer-Price My First Pokedex</h1>
+                <input value={this.state.pokemonName} onChange={(e) => {this.setState({pokemonName: e.target.value.toLowerCase()})}} />
+                <div>
+                <img src={this.state.pokemonImage}/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Pokedex
